285e8f8873121859618c87f83ee7fe36ba8036f1
marcel <marcel@klebauter.de>
2022-08-01 19:51:23 +0200

    new words

--- src/words/wordlist.ts
+++ src/words/wordlist.ts
@@ -1,11 +1,24 @@
 const WordList: string[] = [
-  "Bordstein",
-  "Programmieren",
-  "Territorium",
-  "Wanduhr",
-  "Haushaltskasse",
-  "Batman",
-  "Kinofilm"
+  'Bordstein',
+  'Programmieren',
+  'Territorium',
+  'Wanduhr',
+  'Haushaltskasse',
+  'Batman',
+  'Kinofilm',
+  'Baumhaus',
+  'Autobahn',
+  'Krankenhaus',
+  'Horrorfilm',
+  'Aktiengesellschaft',
+  'Kleiderschrank',
+  'Bankraub',
+  'Geldinstitut',
+  'Mahlzeit',
+  'Fehlerkorrektur',
+  'Feedback',
+  'Gitarrenspieler',
+  'Schlagzeug',
 ];
 
 export default WordList;
